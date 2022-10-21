af0f412e6521e7f5122f2e63eb75085c89102686
Theophilus Lincoln <lincolntheop@gmail.com>
2022-10-21 23:09:13 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express=require('express')
+
+const app=express()
+
+//set view engine
+app.set('view engine','ejs')
+
+//use to server static files like images
+app.use(express.static('public'))
+
+//render main page
+app.get('/',(req,res)=>{
+     res.render('index')
+})
+
+const port=process.env.PORT||3400;
+app.listen(port,()=>{console.log(`Listening on http://localhost/${port}...`)})
